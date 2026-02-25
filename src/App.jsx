63037c5d5219b150d00ffce4c4311610949aade2
Gangapurapu Sravani <sravani2817@gmail.com>
2026-02-25 07:36:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App(){
+  return(
+    <div style={{ textAlign: "left", marginTop: "50px"}}>
+      <h1>Welcome to my React App</h1>
+      <p>This is my first deployed React project</p>
+    </div>
+  );
+};
+export default App;
